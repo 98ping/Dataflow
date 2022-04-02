b48025b2248398c86c444d4eb6fe6b4b7a59c96a
98ping <67809373+98ping@users.noreply.github.com>
2022-04-01 18:24:25 -0700

    update classutil

--- mongo/src/main/kotlin/ltd/matrixstudios/mongo/clazz/ClassUtil.kt
+++ mongo/src/main/kotlin/ltd/matrixstudios/mongo/clazz/ClassUtil.kt
@@ -1,56 +1,68 @@
 package ltd.matrixstudios.mongo.clazz
 
 
+import sun.net.www.protocol.file.FileURLConnection
 import java.io.File
-import java.io.FileInputStream
-import java.util.*
+import java.net.JarURLConnection
+import java.net.URL
+import java.net.URLConnection
+import java.net.URLDecoder
 import java.util.jar.JarEntry
 import java.util.jar.JarInputStream
 
 
 object ClassUtil {
 
-    fun getClassesInPackage(packageName: String): List<Class<*>> {
-        val path = packageName.replace("\\.".toRegex(), File.separator)
-        val classes: MutableList<Class<*>> = ArrayList()
-        val classPathEntries = System.getProperty("java.class.path").split(
-            System.getProperty("path.separator")
-        ).toTypedArray()
-        var name: String
-        for (classpathEntry in classPathEntries) {
-            if (classpathEntry.endsWith(".jar")) {
-                val jar = File(classpathEntry)
-                try {
-                    val `is` = JarInputStream(FileInputStream(jar))
-                    var entry: JarEntry
-                    while (`is`.nextJarEntry.also { entry = it } != null) {
-                        name = entry.name
-                        if (name.endsWith(".class")) {
-                            if (name.contains(path) && name.endsWith(".class")) {
-                                var classPath = name.substring(0, entry.name.length - 6)
-                                classPath = classPath.replace("[\\|/]".toRegex(), ".")
-                                classes.add(Class.forName(classPath))
-                            }
+    private fun checkDirectory(directory: File, packageName: String, classes: ArrayList<Class<*>>) {
+        var tmpDirectory: File
+        if (directory.exists() && directory.isDirectory) {
+            val files = directory.list()
+            if (files != null) {
+                for (file in files) {
+                    if (file.endsWith(".class")) {
+                        try {
+                            classes.add(Class.forName(packageName + '.' + file.substring(0, file.length - 6)))
+                        } catch (ignored: NoClassDefFoundError) {
                         }
+                    } else if (File(directory, file).also { tmpDirectory = it }.isDirectory) {
+                        checkDirectory(tmpDirectory, "$packageName.$file", classes)
                     }
-                } catch (ex: Exception) {
-                    // Silence is gold
-                }
-            } else {
-                try {
-                    val base = File(classpathEntry + File.separatorChar + path)
-                    for (file in base.listFiles()) {
-                        name = file.name
-                        if (name.endsWith(".class")) {
-                            name = name.substring(0, name.length - 6)
-                            classes.add(Class.forName("$packageName.$name"))
-                        }
-                    }
-                } catch (ex: Exception) {
-                    // Silence is gold
                 }
             }
         }
+    }
+
+    private fun checkJarFile(connection: JarURLConnection, packageName: String, classes: ArrayList<Class<*>>) {
+        val jarFile = connection.jarFile
+        val entries = jarFile.entries()
+        var name: String
+        var jarEntry: JarEntry? = null
+        while (entries.hasMoreElements() && entries.nextElement().also { jarEntry = it } != null) {
+            name = jarEntry!!.name
+            if (name.contains(".class")) {
+                name = name.substring(0, name.length - 6).replace('/', '.')
+                if (name.contains(packageName)) {
+                    classes.add(Class.forName(name))
+                }
+            }
+        }
+    }
+
+    fun getClassesInPackage(packageName: String): ArrayList<Class<*>> {
+        val classes = ArrayList<Class<*>>()
+        val cld = Thread.currentThread().contextClassLoader ?: throw ClassNotFoundException("Can't get class loader.")
+        val resources = cld.getResources(packageName.replace('.', '/'))
+        var connection: URLConnection
+        var url: URL? = null
+        while (resources.hasMoreElements() && resources.nextElement().also { url = it } != null) {
+            connection = url!!.openConnection()
+            if (connection is JarURLConnection) {
+                checkJarFile(connection, packageName, classes)
+            } else if (connection is FileURLConnection) {
+                checkDirectory(File(URLDecoder.decode(url!!.path, "UTF-8")), packageName, classes)
+            }
+        }
+
         return classes
     }
 }

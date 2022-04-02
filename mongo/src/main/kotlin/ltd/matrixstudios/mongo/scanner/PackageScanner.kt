791271344f8e5d7c2d133e5817aa2a42e87f3b58
98ping <67809373+98ping@users.noreply.github.com>
2022-04-01 20:45:43 -0700

    debug to try and fix scanning issues

--- mongo/src/main/kotlin/ltd/matrixstudios/mongo/scanner/PackageScanner.kt
+++ mongo/src/main/kotlin/ltd/matrixstudios/mongo/scanner/PackageScanner.kt
@@ -9,6 +9,8 @@ object PackageScanner {
     var annotations = hashMapOf<Class<*>, String>()
 
     fun get(clazz: Class<*>) : String {
+        println(annotations.values.toString())
+        print(clazz.name)
         val finalString = annotations[clazz]
             ?: throw ClassNotStoredException("class was unable to be found in the annotation map")
 
